fe9061479f4ca82ace9601fd5b06c8b45bf86cf4
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-03 06:05:39 +0300

    Create the image model

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
+
 
 # Create your models here.
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=100)
+    caption= models.TextField(max_length=300)
+    pub_date = models.DateTimeField(auto_now_add=True,null=True)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self,caption):
+        self.caption = caption
+        self.save()
