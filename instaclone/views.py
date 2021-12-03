9e675c49ecae8151fc76ecd91d0daa830628e7d2
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-03 06:09:16 +0300

    Create the index view function

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from instaclone.models import Image
+
 
 # Create your views here.
+def index(request):
+  photo = Image.objects.all().order_by('-id')
+
+  return render(request, 'all-photos/index.html',{'photo':photo})
+
