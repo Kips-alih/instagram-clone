55a0d98a83e1fecc6fcf0d89ba225efc86af2d6b
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-06 05:35:32 +0300

    Register the models

--- instaclone/admin.py
+++ instaclone/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from instaclone.models import Image
+from instaclone.models import Image,Profile,Comment,Likes
 
 
 # Register your models here.
 admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Likes)
+
+
+
