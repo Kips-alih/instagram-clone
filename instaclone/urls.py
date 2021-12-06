e57752f05dab04f0071f808413afe520956fb822
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-06 05:36:05 +0300

    Define the application urls

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
   url(r'^$',views.index,name= 'index'),
+  url('profile/', views.profile, name='profile'),
+  url(r'^search/', views.search_results, name='search_results'),
+  url(r'^new/image$', views.new_image, name='newImage'),
+  url(r'^image/',views.image,name ='image'),
+
+
 ]
