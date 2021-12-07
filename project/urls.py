8b37bb8d026798390a984aa7df4a79d047a92770
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-07 04:48:21 +0300

    Make changes in the project urls

--- project/urls.py
+++ project/urls.py
@@ -13,15 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.conf.urls import url,include
 from django.contrib.auth import views 
-
+from django.contrib import admin
+from django.urls import path,include
+from django_registration.backends.one_step.views import RegistrationView
 
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'',include('instaclone.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$', views.logout, {"next_page": '/'}),
+    path('admin/', admin.site.urls),
+    path('',include('instaclone.urls')),
+    path('accounts/register/', RegistrationView.as_view(success_url='/'),
+         name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.logout_then_login, name='logout'), 
+    
+    
 ]
+
+
