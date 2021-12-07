96159192877d7dc670d9b74ac9dc7a9db9c4170c
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-12-07 06:29:06 +0300

    Modify profile form

--- instaclone/forms.py
+++ instaclone/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.db.models import fields
 from django.forms.models import ModelForm
+from django.forms.widgets import Textarea
 from .models import Image,Profile,Comment
 
 class NewImageForm(forms.ModelForm):
@@ -15,6 +16,9 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio','profile_pic']
+        widgets = {
+            'bio': Textarea(attrs={'cols': 30, 'rows': 3}),
+        }
 
 
 class CommentForm(forms.ModelForm):
